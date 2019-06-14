b97eebc386ef631f66903484686d108efae4542b
Dale-Cox91 <50583281+Dale-Cox91@users.noreply.github.com>
2019-06-14 12:03:36 +0100

    first commit

---
+++ JAVA - OfflineExercises/Adventure/src/main/java/Nationwide/Adventure/Adventure.java
@@ -0,0 +1,29 @@
+package Nationwide.Adventure;
+
+import java.util.Scanner;
+
+public class Adventure {
+
+	public static void main(String[] args) {
+		Scanner user_input = new Scanner(System.in);
+		System.out.println("You awaken to find yourself in a barren moor, try 'Look'");
+		System.out.println("Grey foggy clouds float oppressively close to you, reflected in the murky grey water which reaches up your shins");
+       	System.out.println("Some black plants barely poke out of the shallow water.");
+       	System.out.println("Try 'North', 'East', 'South' or 'West'");
+       	System.out.println("You notice a small instrument in your hand, it appears to be watchlike in origin, but it does not tell the time");
+       	if user input equals North{
+       	System.out.println("The dial reads 5m");
+       	}
+       	if user input equals North{
+       	System.out.println("The dial reads 2.5m");
+       	}
+       	if user input equals North{
+       	System.out.println("You find a box filled with treasure, you win the game!!");
+       	}
+       	else user input equals South, East, West{
+       	System.out.println("You went the wrong way and were devoured by the dark, game over");
+       	}
+        }
+	}
+
+
